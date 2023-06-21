f54ff64f762008b0ca70922cd1aa006858bc7eaf
FarhanTI05 <githubbima73@gmail.com>
2023-06-21 15:08:52 +0700

    tugas P8-12

---
+++ resources/views/toko/profile.blade.php
@@ -0,0 +1,25 @@
+@extends('template/main')
+
+@section('content')
+<div class="row">
+    <div class="col-md-8">
+        <h1>Sepatu SPRINT-X Pro</h1>
+        <P>
+
+Deskripsi: 
+
+Tingkatkan performa Anda dalam olahraga dengan Sepatu SPRINT-X Pro yang revolusioner. Dirancang khusus untuk atlet profesional dan pecinta olahraga yang ambisius, sepatu ini menggabungkan teknologi terdepan dan desain yang ergonomis untuk memberikan kinerja yang tak tertandingi.
+
+Dibuat dengan menggunakan bahan berkualitas tinggi, Sepatu SPRINT-X Pro memberikan kenyamanan luar biasa dan penampilan yang stylish. Upper sepatu terbuat dari serat sintetis yang ringan dan tahan lama, memberikan keleluasaan gerakan tanpa mengorbankan kestabilan. Sistem ventilasi yang canggih juga memastikan sirkulasi udara yang optimal, menjaga kaki Anda tetap segar dan kering selama aktivitas fisik.
+
+Bagian dalam sepatu dilengkapi dengan bantalan yang empuk dan responsif, memberikan dukungan yang sempurna untuk setiap langkah. Teknologi penguat canggih pada bagian tumit dan jari kaki menjaga keseimbangan dan mengurangi risiko cedera. Sol yang dibuat dengan teknologi khusus memberikan traksi yang luar biasa, memungkinkan Anda bergerak dengan kecepatan tinggi dan menghadapi berbagai permukaan tanah dengan percaya diri.
+
+Tidak hanya memberikan kinerja yang superior, Sepatu SPRINT-X Pro juga memiliki estetika yang menarik. Desain modern dan garis-garis yang dinamis memberikan kesan yang futuristik dan menawan. Tersedia dalam berbagai pilihan warna yang trendy, Anda dapat menemukan sepatu yang sesuai dengan gaya pribadi Anda.
+
+Dengan Sepatu SPRINT-X Pro, tidak ada batasan untuk mencapai performa terbaik Anda. Apakah Anda seorang pelari, pemain basket, atau pecinta kegiatan olahraga lainnya, sepatu ini akan menjadi mitra setia Anda dalam mengejar kesuksesan. Dapatkan sepasang sepatu ini sekarang dan rasakan perbedaannya dalam setiap langkah Anda.</P>
+    </div>
+    <div class="col-md-4">
+        <img src="img/foto4" class="w-100" alt="foto profile">
+    </div>
+</div>
+@endsection
